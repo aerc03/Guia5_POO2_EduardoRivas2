d34ea04990ebc3bb22a479effa9f1fc8c5cc9870
eduardo <eduardo@aerc>
2016-09-11 18:27:53 -0600

    Ya guarda, pero no consulta ni nada mas

---
+++ Guia5/src/main/java/com/sv/udb/controlador/AlumnosBean.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sv.udb.controlador;
+
+import com.sv.udb.modelo.Alumnos;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import org.primefaces.context.RequestContext;
+
+/**
+ *
+ * @author REGISTRO
+ */
+@Named(value = "alumnosBean")
+@ViewScoped
+public class AlumnosBean implements Serializable{
+    private Alumnos objeAlum;
+    private List<Alumnos> listAlum;
+    private boolean guardar;
+
+    public Alumnos getObjeAlum() {
+        return objeAlum;
+    }
+
+    public void setObjeAlum(Alumnos objeAlum) {
+        this.objeAlum = objeAlum;
+    }
+
+    public boolean isGuardar() {
+        return guardar;
+    }
+
+    public List<Alumnos> getListAlum() {
+        return listAlum;
+    }
+    
+    /**
+     * Creates a new instance of AlumnosBean
+     */
+    
+    public AlumnosBean() {
+    }
+    
+    @PostConstruct
+    public void init()
+    {
+        this.objeAlum = new Alumnos();
+        this.guardar = true;
+        this.consTodo();
+    }
+    public void limpForm()
+    {
+        System.err.println("entyra");
+        this.objeAlum = new Alumnos();
+        this.guardar = true;        
+    }
+    public void guar()
+    {
+        RequestContext ctx = RequestContext.getCurrentInstance(); //Capturo el contexto de la página
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Guia5");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try
+        {
+            em.persist(this.objeAlum);
+            tx.commit();
+            this.guardar = true;
+            this.listAlum.add(this.objeAlum); //Agrega el objeto a la lista para poderse mostrar en tabla
+            this.objeAlum = new Alumnos(); // Limpiar
+            ctx.execute("setMessage('MESS_SUCC', 'Atención', 'Datos guardados')");
+        }
+        catch(Exception ex)
+        {
+            ctx.execute("setMessage('MESS_ERRO', 'Atención', 'Error al guardar ')");
+            tx.rollback();
+        }
+        finally
+        {
+            em.close();
+            emf.close();            
+        }
+    }
+    
+    public void consTodo()
+    {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Guia5");
+        EntityManager em = emf.createEntityManager();
+        try
+        {
+            this.listAlum = em.createNamedQuery("Alumnos.findAll", Alumnos.class).getResultList();
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            em.close();
+            emf.close();            
+        }
+    }
+}
